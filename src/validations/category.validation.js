da4c813e8bcb646561d50ac79cb6cdd3579d762a
Arya Priema <99aryaprima@gmail.com>
2023-10-03 19:23:49 +0700

    feat: validations - category and post

---
+++ src/validations/category.validation.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const createCategory = {
+  body: Joi.object().keys({
+    name: Joi.string().required().max(30),
+  }),
+};
+
+const getCategories = {
+  params: Joi.object().keys({
+    categoryId: Joi.string().optional().custom(objectId),
+  }),
+};
+
+const deleteCategory = {
+  params: Joi.object().keys({
+    categoryId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createCategory,
+  getCategories,
+  deleteCategory,
+};

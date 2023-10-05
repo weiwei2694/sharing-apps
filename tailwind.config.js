6d4ba4461df5b24002c6889ead5256d13907f0cc
Arya Priema <99aryaprima@gmail.com>
2023-10-05 16:43:56 +0700

    switching from bootstrap to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/views/**/*.ejs"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  ],
+};
